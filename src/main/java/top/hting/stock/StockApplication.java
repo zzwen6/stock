64a748c6f3e503f1bf052acdbbd91be159f8f51d
zzwen6 <1104340881@qq.com>
2026-01-06 17:53:59 +0800

    Update comment in StockApplication.java

--- src/main/java/top/hting/stock/StockApplication.java
+++ src/main/java/top/hting/stock/StockApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-// 增加点注释触发22
+// 增加点注释触发2222
 @SpringBootApplication
 @EnableFeignClients({"top.hting.stock.api"})
 @Slf4j
